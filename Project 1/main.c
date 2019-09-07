0f7b953eeeb343dc0bb02c5b5f59f3b690a4addb
Yoo (Daniel) Choi <dchoiboi0)@gmail.com>
2019-09-06 22:56:50 -0400

    Initial Commit

---
+++ Project 1/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Project 1
+//
+//  Created by Daniel Choi on 9/6/19.
+//  Copyright © 2019 Daniel Choi. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
